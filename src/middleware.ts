8e3b5e4dd54372fea21a861e4ed62326aa341682
Xiel7 <ericng402@gmail.com>
2022-08-14 23:19:40 +0800

    it works on netlify but not on vercel for some reason

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,20 @@
-import { withAuth } from "next-auth/middleware"
+// import { withAuth } from "next-auth/middleware"
 
-export default withAuth(
-  // `withAuth` augments your `Request` with the user's token.
-  function middleware(req) {
-    console.log(req.nextauth.token)
+// export default withAuth(
+//   // `withAuth` augments your `Request` with the user's token.
+//   function middleware(req) {
+//     console.log(req.nextauth.token)
     
-  },
-  {
-    callbacks: {
-      authorized: async ({ token }) => {
-        console.log("middleware", token);
-        // return token?.role !== "admin";
-        return true;
-      }
-    },
-  }
-)
-// export { default } from "next-auth/middleware"
-// export const config = { matcher: ["/main"] }
+//   },
+//   {
+//     callbacks: {
+//       authorized: async ({ token }) => {
+//         console.log("middleware", token);
+//         // return token?.role !== "admin";
+//         return true;
+//       }
+//     },
+//   }
+// )
+export { default } from "next-auth/middleware"
+export const config = { matcher: ["/main"] }
